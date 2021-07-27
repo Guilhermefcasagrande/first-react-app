5e86fb13972b6efd11980fea5ca28010ca034fed
Guilherme <guilherme.casagrande@arealocal.com.br>
2021-07-27 20:45:28 -0300

    Primeiro contato com react

--- App.js
+++ App.js
@@ -1,12 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [nome, setNome] = useState('Guilherme');
+  const testes = () =>{
+    alert('Teste');
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <StatusBar style="auto" />
+        <TouchableOpacity onPress={() => setNome('Francisco')} style={styles.btn}>
+          <Text style={{color: 'white', textAlign: 'center'}}>Clique aqui!</Text>
+        </TouchableOpacity>
+        <Text>{nome}</Text>
+        <Image source={{ uri: "https://i.imgur.com/TkIrScD.png" }} style={{ width: 305, height: 159 }} />
     </View>
   );
 }
@@ -18,4 +27,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  btn:{
+    width: 100,
+    padding: 10,
+    backgroundColor: 'green',
+  }
 });
